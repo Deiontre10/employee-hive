6330075e1435531f1aabd0031d21758da16e55ba
Deiontre10 <travontazlowry@gmail.com>
2023-01-09 15:43:58 -0500

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,21 +6,21 @@ INSERT INTO department (name)
     ("Legal");
 INSERT INTO role (title, salary, department_id) 
     VALUES 
-    ("Sales Lead"),
-    ("Salesperson"),
-    ("Lead Engineer"),
-    ("Software Engineer"),
-    ("Account Manager"),
-    ("Accountant"),
-    ("Legal Team Lead"),
-    ("Lawyer");
+    ("Sales Lead", 120000, 1),
+    ("Salesperson", 100000,1),
+    ("Lead Engineer", 180000,2),
+    ("Software Engineer", 140000,2),
+    ("Account Manager", 160000,3),
+    ("Accountant", 130000,3),
+    ("Legal Team Lead", 120000,4),
+    ("Lawyer", 110000,4);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
     VALUES 
-    ("Professor", "Oak"),
-    ("Ash", "Ketchum"),
-    ("Shoto", "Todoroki"),
-    ("Izuku", "Midoriya"),
-    ("Sasuke", "Uchiha"),
-    ("Naruto", "Uzumaki"),
-    ("Killua", "Zoldyck"),
-    ("Gon", "Freecss");
+    ("Professor", "Oak", 1, null),
+    ("Ash", "Ketchum", 2, 1),
+    ("Shoto", "Todoroki", 3, null),
+    ("Izuku", "Midoriya", 4, 3),
+    ("Sasuke", "Uchiha", 5, null),
+    ("Naruto", "Uzumaki", 6, 5),
+    ("Killua", "Zoldyck", 7, null),
+    ("Gon", "Freecss", 8, 7);
